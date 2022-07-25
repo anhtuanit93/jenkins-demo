61535d26cf3aa4bc3dffaaf2e90b1757ecc5c241
Tuan Tran 93 <80957567+anhtuanit93@users.noreply.github.com>
2022-07-25 20:19:55 +0700

    Add files via upload

---
+++ jenkins/deploy/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo maven-project > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+echo $PASS >> /tmp/.auth
+
+scp  /tmp/.auth root@192.168.150.128:/tmp/.auth
+scp ./pipeline6/jenkins/deploy/publish root@113.164.246.20:/tmp/publish
+ssh root@192.168.150.128 "/tmp/publish"
