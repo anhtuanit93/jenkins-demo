434431534a65c3b89df62518d038034bf38fd3a7
Tuan Tran 93 <80957567+anhtuanit93@users.noreply.github.com>
2022-07-25 21:10:50 +0700

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,4 +6,4 @@ echo $PASS >> /tmp/.auth
 
 scp  /tmp/.auth root@192.168.150.128:/tmp/.auth
 scp ./jenkins/deploy/publish root@192.168.150.128:/tmp/publish
-ssh root@192.168.150.128 "/tmp/publish"
+ssh root@192.168.150.128 "/bin/bash /tmp/publish"
