28b159471520b3025f6ae2489a9236ae597abad1
Tuan Tran 93 <80957567+anhtuanit93@users.noreply.github.com>
2022-07-25 21:20:57 +0700

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp  /tmp/.auth root@192.168.150.128:/tmp/.auth
+scp /tmp/.auth root@192.168.150.128:/tmp/.auth
 scp ./jenkins/deploy/publish root@192.168.150.128:/tmp/publish
 ssh root@192.168.150.128 "/bin/bash /tmp/publish"
